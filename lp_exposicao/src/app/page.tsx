eca906b5f31b58b7844ba5a05c8afe4249799805
Iago Ramone <iagoramone.fisica@gmail.com>
2025-02-19 17:39:29 -0300

    Alteração na segunda seção

--- lp_exposicao/src/app/page.tsx
+++ lp_exposicao/src/app/page.tsx
@@ -34,32 +34,71 @@ function SectionOne() {
   );
 }
 
+
 function SectionTwo() {
   return (
-    <div className="flex flex-col items-center min-h-screen bg-gray-100 text-gray-900 py-12 px-6">
-      <main className="flex flex-col lg:flex-row items-center max-w-6xl w-full text-center lg:text-left">
-        
-        <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 flex-1">
-          <Image src="/pratos/foto_adez.jpg" alt="Copo com laranja" width={200} height={200} className="rounded-lg shadow-md" />
-          <Image src="/pratos/flor_no_copo.jpg" alt="Flor no copo" width={200} height={200} className="rounded-lg shadow-md" />
-          <Image src="/pratos/foto_adez.jpg" alt="Foto Adez" width={200} height={200} className="rounded-lg shadow-md" />
-          <Image src="/art4.jpg" alt="Obra 4" width={200} height={200} className="rounded-lg shadow-md" />
-          <Image src="/art5.jpg" alt="Obra 5" width={200} height={200} className="rounded-lg shadow-md" />
-          <Image src="/art6.jpg" alt="Obra 6" width={200} height={200} className="rounded-lg shadow-md" />
+    <div className="flex flex-col items-center min-h-screen bg-gray-100 text-gray-900 py-12 px-6 space-y-12">
+      
+      <div className="flex flex-col lg:flex-row items-center max-w-6xl w-full">
+        <div className="flex-1">
+          <Image 
+            src="/pratos/Fasano.jpg" 
+            alt="Fasano" 
+            width={400} 
+            height={300} 
+            className="rounded-lg shadow-md" 
+          />
         </div>
-
-        <div className="flex-1 lg:pl-12 mt-8 lg:mt-0">
+        <div className="flex-1 lg:pl-12 mt-8 lg:mt-0 text-center lg:text-left">
           <h2 className="text-2xl font-semibold mb-4">Explore a Arte do Futuro</h2>
           <p className="text-lg text-gray-700">
-            Descubra obras de artistas inovadores e experimente a nova era da arte digital e tradicional. 
+          A sofisticação da alta gastronomia e a arte da fotografia se encontram na exposição - Nome da Exposição -, uma experiência visual única que transforma pratos em verdadeiras obras de arte. Com produção assinada pelo Estúdio 10, a mostra reúne imagens impactantes que exploram a estética dos alimentos, elevando a fotografia gastronômica a um novo patamar.
           </p>
         </div>
+      </div>
 
-      </main>
-      
+      <div className="flex flex-col lg:flex-row-reverse items-center max-w-6xl w-full">
+        <div className="flex-1">
+          <Image 
+            src="/pratos/lendas_naval.jpg" 
+            alt="Lendas Naval" 
+            width={400} 
+            height={300} 
+            className="rounded-lg shadow-md" 
+          />
+        </div>
+        <div className="flex-1 lg:pr-12 mt-8 lg:mt-0 text-center lg:text-left">
+
+          <p className="text-lg text-gray-700">
+          A noite de inauguração contará com a presença de grandes nomes da cena gastronômica e cultural, como o prestigiado chef João Diamante, que realizará uma exclusiva noite de autógrafos. Além disso, marcas icônicas como Heaven e Fasano marcam presença, agregando ainda mais requinte ao evento.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col lg:flex-row items-center max-w-6xl w-full">
+        <div className="flex-1">
+          <Image 
+            src="/pratos/Vegue-se.JPG" 
+            alt="Vegue-se" 
+            width={400} 
+            height={300} 
+            className="rounded-lg shadow-md" 
+          />
+        </div>
+        <div className="flex-1 lg:pl-12 mt-8 lg:mt-0 text-center lg:text-left">
+          <p className="text-lg text-gray-700">
+          Mais do que uma celebração da beleza dos alimentos, a exposição também tem um propósito social: apoio ao projeto “Diamantes na Cozinha”, um projeto dedicado à transformação de vidas por meio da gastronomia. O evento reforça o compromisso com a inclusão e o impacto social positivo, trazendo a arte como ferramenta de mudança.
+
+A mostra acontece no Vogue Square, com abertura no dia 11/03 e permanece em cartaz até 25/04. Uma oportunidade imperdível para apreciar o encontro entre gastronomia, fotografia e exclusividade.
+          </p>
+        </div>
+      </div>
+
     </div>
   );
 }
+
+
 
 
 function SectionThree() {
